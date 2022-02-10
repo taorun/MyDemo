0298ba34e410aa8d0de72373c5abdc411f13ebf0
runtao <runtao@RYAN-WORKSTATIO>
2022-02-10 15:22:14 +0800

    Add project files.

---
+++ ASPWebAPIDemo/Controllers/CatController.cs
@@ -0,0 +1,37 @@
+﻿using ASPWebAPIDemo.Models;
+using ASPWebAPIDemo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebAPIDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Cat>> GetAll() => CatService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Cat> Get(int id) => CatService.GetAll().FirstOrDefault(x => x.Id == id);
+
+        [HttpPost]
+        public IActionResult Create(Cat cat)
+        {
+            if(cat == null)
+            {
+                return BadRequest();
+            }
+
+            CatService.Add(cat);
+            return CreatedAtAction(nameof(Create), new {id = cat.Id}, cat);
+        }
+
+        /*[HttpPost(Name = "Create2")]
+        public IActionResult Create2(Cat cat)
+        {
+            CatService.Add(cat);
+            return CreatedAtAction(nameof(Create), new { id = cat.Id }, cat);
+        }*/
+    }
+}
---
+++ ASPWebAPIDemo/Services/CatService.cs
@@ -0,0 +1,19 @@
+﻿using ASPWebAPIDemo.Models;
+
+namespace ASPWebAPIDemo.Services
+{
+    public static class CatService
+    {
+        static List<Cat> Cats { get; }
+        static CatService()
+        {
+            Cats = new List<Cat> { };
+        }
+
+        public static List<Cat> GetAll() => Cats;
+        public static void Add(Cat cat)
+        {
+            Cats.Add(cat);
+        }
+    }
+}
