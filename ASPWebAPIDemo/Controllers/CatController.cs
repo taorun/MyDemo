55b487c85bd35a7c07d8d74b403b6fa087278415
Run Tao <runtao@microsoft.com>
2022-03-24 16:29:43 +0800

    add keyvault secrets

--- ASPWebAPIDemo/Controllers/CatController.cs
+++ ASPWebAPIDemo/Controllers/CatController.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Configuration;
 
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
 namespace ASPWebAPIDemo.Controllers
 {
     [Route("[controller]")]
@@ -24,7 +28,26 @@ namespace ASPWebAPIDemo.Controllers
         public ActionResult<Cat> Get(int id) => CatService.GetAll().FirstOrDefault(x => x.Id == id);
         [HttpGet]
         [Route("mycfg")]
-        public ActionResult<String> GetConfig() => configuration.GetSection("key1").Value;
+        public ActionResult<String> GetConfig()
+        {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                 }
+            };
+
+            var client = new SecretClient(new Uri("https://ryandeploykeyvault.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("mykeyvaultkey");
+
+            string secretValue = secret.Value;
+            return $"Appsettings: {configuration.GetSection("key1").Value} Secret:{secretValue}";
+        }
 
         [HttpPost]
         public IActionResult Create(Cat cat)
